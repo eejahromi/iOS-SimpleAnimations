4d8607d64a2724b41a3c1543e779a2e0c2d69746
Ehsan Jahromi <ehsan.enj@gmail.com>
2017-09-30 23:11:25 -0700

    added XCode project

---
+++ CheckMark/CheckMark/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CheckMark
+//  Copyright © 2017 Ehsan Jahromi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
---
+++ CheckMark/CheckMark/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CheckMark
+//  Copyright © 2017 Ehsan Jahromi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
+
