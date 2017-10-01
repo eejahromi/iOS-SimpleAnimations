0f628c24efb16f1021f070715a551504847ac57e
Ehsan Jahromi <ehsan.enj@gmail.com>
2017-10-01 15:54:05 -0700

    added checkmark path and animation

--- CheckMark/CheckMark/ViewController.swift
+++ CheckMark/CheckMark/ViewController.swift
@@ -8,6 +8,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var pathAnimation: CABasicAnimation!
+    let pathLayer = CAShapeLayer()
+        
     var checkBox:UIView = {
         let box = UIView()
         box.frame = CGRect(x: 0.0, y: 0.0, width: 150.0, height: 150.0)
@@ -16,10 +19,11 @@ class ViewController: UIViewController {
         box.backgroundColor = UIColor(red: 1, green: 81, blue: 134)
         return box
     }()
-
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pathAnimation = CABasicAnimation(keyPath: "strokeEnd")
 
         checkBox.center = view.center
         view.addSubview(checkBox)
@@ -27,11 +31,38 @@ class ViewController: UIViewController {
         createPath()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0 , execute: {
+            self.checkBox.layer.addSublayer(self.pathLayer)
+            self.pathLayer.strokeEnd = 1.0
+            self.pathLayer.removeAllAnimations()
+            self.pathLayer.add(self.pathAnimation, forKey:"strokeEnd")
+        })
+    }
+    
     private func createPath() {
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: 10, y: 110 * 3 / 5))
+        path.move(to: CGPoint(x: 20, y: 120 * 3 / 5))
         path.addLine(to: CGPoint(x: 100 / 2, y: 110))
-        path.addLine(to: CGPoint(x: 140, y: 20))
+        path.addLine(to: CGPoint(x: 120, y: 40))
+        
+        pathLayer.frame = checkBox.bounds
+        pathLayer.path = path.cgPath
+        pathLayer.strokeColor = UIColor.white.cgColor
+        pathLayer.fillColor = nil
+        pathLayer.lineWidth = 12
+        pathLayer.lineJoin = kCALineJoinRound
+        pathLayer.lineCap = kCALineCapRound
+        
+        addAnimation(animation: CABasicAnimation(keyPath:"strokeEnd"), from: 0, to: 1)
+    }
+    
+    private func addAnimation(animation: CABasicAnimation, duration: CFTimeInterval = 0.5, from: Double, to: Double) {
+        pathAnimation.duration = duration
+        pathAnimation.fromValue = NSNumber(floatLiteral: from)
+        pathAnimation.toValue = NSNumber(floatLiteral: to)
     }
 
 }
